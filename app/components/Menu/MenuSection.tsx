19bb0504f11b6e13ff55f26b1bfbfe3460d7f5a6
Kashif <kashifsaeed8856@gmail.com>
2026-08-18 21:12:49 +0500

    style: standardize MenuSection vertical gap and restore grid gap between dual images

--- app/components/Menu/MenuSection.tsx
+++ app/components/Menu/MenuSection.tsx
@@ -16,7 +16,7 @@ export const MenuSection = ({
   className = ""
 }: MenuSectionProps) => {
   return (
-    <section id={id} className={`relative w-full py-6 lg:py-8 bg-[#0a0a0a] ${className}`}>
+    <section id={id} className={`relative w-full py-6 lg:py-12 bg-[#0a0a0a] ${className}`}>
       {/* Background Texture */}
       <div 
         className={`absolute inset-0 z-0 ${bgOpacity} pointer-events-none`}
