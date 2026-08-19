0053843d779177c2aad5beb7b8d7063227e9c5fc
Kashif <kashifsaeed8856@gmail.com>
2026-08-19 17:55:49 +0500

    Revert MenuDualImage.tsx desktop layout to previous state as requested

--- app/components/Menu/MenuDualImage.tsx
+++ app/components/Menu/MenuDualImage.tsx
@@ -22,28 +22,28 @@ export const MenuDualImage = ({
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, margin: '-80px' }}
         transition={{ duration: 0.8, ease: 'easeOut' }}
-        className={`w-full flex flex-col md:flex-row items-center justify-between lg:pr-0 ${(mobileLeftImageSrc || mobileRightImageSrc) ? 'py-4 lg:pt-0 lg:pb-0' : 'py-0 lg:pt-0 lg:pb-0'}`}
+        className={`w-full flex flex-col md:flex-row items-center justify-center lg:pr-0 ${(mobileLeftImageSrc || mobileRightImageSrc) ? 'py-4 lg:pt-0 lg:pb-0' : 'py-0 lg:pt-0 lg:pb-0'}`}
       >
         {/* Desktop Left Image */}
-        <div className="relative w-1/2 h-[200px] sm:h-[250px] md:h-[350px] lg:h-[500px] xl:h-[600px] z-10 hidden md:block">
+        <div className="relative w-[55%] h-[200px] sm:h-[250px] md:h-[350px] lg:h-[500px] xl:h-[600px] z-10 hidden md:block">
           <Image
             src={leftImageSrc}
             alt="Menu Item Left"
             fill
-            sizes="(max-width: 768px) 100vw, 50vw"
-            className="object-contain object-left drop-shadow-2xl"
+            sizes="(max-width: 768px) 100vw, 60vw"
+            className="object-contain object-bottom md:object-bottom-right scale-[1.15] drop-shadow-2xl"
             priority
           />
         </div>
 
         {/* Desktop Right Image */}
-        <div className="relative w-1/2 h-[200px] sm:h-[250px] md:h-[350px] lg:h-[500px] xl:h-[600px] z-0 hidden md:block">
+        <div className="relative w-[55%] h-[200px] sm:h-[250px] md:h-[350px] lg:h-[500px] xl:h-[600px] z-0 ml-[-15%] hidden md:block">
           <Image
             src={rightImageSrc}
             alt="Menu Item Right"
             fill
-            sizes="(max-width: 768px) 100vw, 50vw"
-            className="object-contain object-right drop-shadow-2xl"
+            sizes="(max-width: 768px) 100vw, 60vw"
+            className="object-contain object-top md:object-bottom-left scale-[1.15] drop-shadow-2xl"
           />
         </div>
 
@@ -57,7 +57,7 @@ export const MenuDualImage = ({
                 width={1000}
                 height={750}
                 sizes="(max-width: 1024px) 50vw, 50vw"
-                className="w-1/2 h-auto object-cover block m-0 p-0 origin-right"
+                className="w-1/2 h-auto object-cover block m-0 p-0 scale-[1.15] origin-right"
               />
             )}
             {mobileRightImageSrc && (
@@ -67,7 +67,7 @@ export const MenuDualImage = ({
                 width={1000}
                 height={750}
                 sizes="(max-width: 1024px) 50vw, 50vw"
-                className="w-1/2 h-auto object-cover block m-0 p-0 origin-left"
+                className="w-1/2 h-auto object-cover block m-0 p-0 scale-[1.15] origin-left"
               />
             )}
           </div>
